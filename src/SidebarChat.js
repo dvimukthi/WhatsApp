55f85997fcd1ed4efcfdaf653d8e70a0721da4dd
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2021-07-22 19:10:41 +0530

    sidebarChat

---
+++ src/SidebarChat.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import { Avatar } from "@material-ui/core";
+import "./SidebarChat.css";
+
+function SidebarChat({ addNewChat }) {
+  const [seed, setSeed] = useState("");
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() * 5000));
+  }, []);
+
+  const createChat = () => {};
+
+  return !addNewChat ? (
+    <div className="sidebarChat">
+      <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+      <div className="sidebar__chats__info">
+        <h2>Room name</h2>
+        <p>Last message...</p>
+      </div>
+    </div>
+  ) : (
+    <div onClick={createChat} className="sidebarChat">
+      <h2>Add New Chat</h2>
+    </div>
+  );
+}
+
+export default SidebarChat;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCXCl1hmkusEXOnX0wp29AvCpdRQ3jr7OM",
+  authDomain: "whatsapp-clone-d1d6e.firebaseapp.com",
+  projectId: "whatsapp-clone-d1d6e",
+  storageBucket: "whatsapp-clone-d1d6e.appspot.com",
+  messagingSenderId: "405083680113",
+  appId: "1:405083680113:web:a77497d6ffe35fc1999ad0",
+  measurementId: "G-85V7Q0RS38",
+};

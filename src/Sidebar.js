619ef89eb25d89c00547647a627511c536008933
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2021-07-24 23:05:47 +0530

    Google acount image

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -7,9 +7,11 @@ import { SearchOutlined } from "@material-ui/icons";
 import "./Sidebar.css";
 import SidebarChat from "./SidebarChat";
 import db from "./firebase";
+import { useStateValue } from "./StateProvider";
 
 function Sidebar() {
   const [rooms, setRooms] = useState([]);
+  const [{ user }, dispatch] = useStateValue();
 
   useEffect(() => {
     const unsubscribe = db
@@ -25,7 +27,7 @@ function Sidebar() {
   return (
     <div className="sidebar">
       <div className="sidebar__header">
-        <Avatar />
+        <Avatar src={user?.photoURL} />
         <div className="sidebar__headerRight">
           <IconButton>
             <DonutLargeIcon />

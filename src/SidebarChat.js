f9024cddc2134df3a64aa56ffd066600da2be92f
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2021-07-25 01:13:42 +0530

    Edit SidebarChat useEffect

--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -18,7 +18,7 @@ function SidebarChat({ id, name, addNewChat }) {
           setMessages(snapshot.docs.map((doc) => doc.data()))
         );
     }
-  }, []);
+  }, [id]);
 
   useEffect(() => {
     setSeed(Math.floor(Math.random() * 5000));

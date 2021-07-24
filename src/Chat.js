dfcc81673ba03863561729cc232df7e9264c70bf
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2021-07-24 14:35:59 +0530

    Chat Room Clickable

--- src/Chat.js
+++ src/Chat.js
@@ -3,11 +3,13 @@ import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
 import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
 import MicIcon from "@material-ui/icons/Mic";
 import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import "./Chat.css";
 
 function Chat() {
   const [input, setInput] = useState("");
   const [seed, setSeed] = useState("");
+  const { roomId } = useParams();
 
   useEffect(() => {
     setSeed(Math.floor(Math.random() * 5000));

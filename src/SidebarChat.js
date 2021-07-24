b24ca749a9fa3afe5c26f686025a375dc8e04489
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2021-07-25 01:50:52 +0530

    SidebarChat font size edit

--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -39,7 +39,7 @@ function SidebarChat({ id, name, addNewChat }) {
     <Link to={`/rooms/${id}`}>
       <div className="sidebarChat">
         <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
-        <div className="sidebar__chats__info">
+        <div className="sidebarChat__info">
           <h2>{name}</h2>
           <p>{messages[0]?.message}</p>
         </div>
